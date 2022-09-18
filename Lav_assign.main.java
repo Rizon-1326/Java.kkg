c22cccc346640fa44bdd6c1e25956582443b2940
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-09-19 01:16:00 +0600

    Create Lav_assign.main.java

---
+++ Lav_assign.main.java
@@ -0,0 +1,17 @@
+package abstraction;
+
+public class Main_call {
+	public static void main(String[] args) {
+		Teacher mentor;
+		
+		mentor =new Student_1();
+		mentor.Class_test();
+		mentor.answer_sheet();
+		
+		mentor =new Student_2();
+		mentor.Class_test();
+		mentor.answer_sheet();
+		
+		
+	}
+}
